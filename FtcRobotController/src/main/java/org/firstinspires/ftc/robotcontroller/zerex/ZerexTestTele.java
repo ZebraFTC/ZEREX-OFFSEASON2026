3dae18d3aecc05dc50fb586b294b02e5c8285d94
ZLT06 <zebolts30416@gmail.com>
2026-06-27 13:14:39 -0400

    works now

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/zerex/ZerexTestTele.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/zerex/ZerexTestTele.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 
-public class ZerexTestTele extends OpMode {
+public class  ZerexTestTele extends OpMode {
 
     private DcMotor frontLeft;
     private DcMotor frontRight;
@@ -27,7 +27,7 @@ public class ZerexTestTele extends OpMode {
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
 
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
     }
@@ -36,12 +36,12 @@ public class ZerexTestTele extends OpMode {
     public void loop() {
 
         drive = -SPEED * gamepad1.left_stick_y; //y inputs are reversed
-        //strafe = SPEED * gamepad1.left_stick_x;
+        strafe = SPEED * gamepad1.left_stick_x;
         turn = SPEED * gamepad1.right_stick_x;
 
-        frontLeft.setPower(drive + turn); // +strafe
-        frontRight.setPower(drive - turn); // - strafe
-        backLeft.setPower(drive + turn); // - strafe
-        backRight.setPower(drive - turn); // + strafe
+        frontLeft.setPower(drive + turn + strafe);
+        frontRight.setPower(drive - turn - strafe); //
+        backLeft.setPower(drive + turn - strafe); //
+        backRight.setPower(drive - turn + strafe); //
     }
 }
