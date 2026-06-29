a045769ac9e7942ba8d1c82237116886cb32c2c2
ZLT06 <zebolts30416@gmail.com>
2026-06-29 19:21:54 -0400

    should score

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/zerex/ZerexTestTele.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/zerex/ZerexTestTele.java
@@ -13,11 +13,17 @@ public class  ZerexTestTele extends OpMode {
     private DcMotor frontRight;
     private DcMotor backRight;
     private DcMotor backLeft;
+    private DcMotor leftArmMotor;
+    private DcMotor rightArmMotor;
+    private DcMotor intakeMotor;
 
     private double drive;
     private double strafe;
     private double turn;
     private final double SPEED = 1.0;
+
+    Neck arm;
+    Mouth intake;
 
 
     @Override
@@ -26,10 +32,16 @@ public class  ZerexTestTele extends OpMode {
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        leftArmMotor = hardwareMap.get(DcMotor.class, "leftArmMotor");
+        rightArmMotor = hardwareMap.get(DcMotor.class, "rightArmMotor");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intake");
 
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        arm = new Neck(leftArmMotor, rightArmMotor);
+        intake = new Mouth(intakeMotor);
     }
 
     @Override
@@ -43,5 +55,30 @@ public class  ZerexTestTele extends OpMode {
         frontRight.setPower(drive - turn - strafe); //
         backLeft.setPower(drive + turn - strafe); //
         backRight.setPower(drive - turn + strafe); //
+
+        if (gamepad2.left_trigger > 0.2){
+            arm.down();
+        } else {
+            arm.stop();
+        }
+
+        if (gamepad2.right_trigger > 0.2){
+            arm.up();
+        } else {
+            arm.stop();
+        }
+
+        if (gamepad2.b){
+            intake.intake();
+        } else {
+            intake.stop();
+        }
+
+        if (gamepad2.a){
+            intake.outtake();
+        } else {
+            intake.stop();
+        }
+
     }
-}
+    }
