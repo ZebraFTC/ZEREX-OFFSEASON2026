81f748a918c0edd3e55026153bb6f9551f50938d
ZLT08 <zebracompetitionteams@gmail.com>
2026-07-30 19:34:37 -0400

    fix auto, teleop is broken(right back wheel)

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/zerex/ZerexTestTele.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/zerex/ZerexTestTele.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.robotcontroller.zerex;
-
+//right back wheel moves backwards for some reason
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
