25ee54316c3b82a27f4e51226fd48fd6c4363682
ZLT08 <zebracompetitionteams@zebra.com>
2026-08-08 11:12:44 -0400

    fixed arm reverse bug

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/zerex/ZerexTestTele.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/zerex/ZerexTestTele.java
@@ -64,10 +64,10 @@ public class  ZerexTestTele extends OpMode {
 
 
         if (gamepad2.left_stick_y > 0.2) {
-            arm.up(0.5);
+            arm.down(0.5);
         }
         else if (gamepad2.left_stick_y < -0.2){
-            arm.down(0.5);
+            arm.up(0.5);
         }
         else {
             arm.hold();
